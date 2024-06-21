2c336d0f9e6928b6d13cadc159ccb3e0c8f0c067
aliwert <154356044+aliwert@users.noreply.github.com>
2024-06-21 19:45:28 +0300

    about section

--- MvcResume/Controllers/DefaultController.cs
+++ MvcResume/Controllers/DefaultController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcResume.Models.Entity;
 
 namespace MvcResume.Controllers
 {
     public class DefaultController : Controller
     {
         // GET: Default
+        DbCvEntities db = new DbCvEntities();
         public ActionResult Index()
         {
-            return View();
+            var degerler = db.TblAbouts.ToList();
+            return View(degerler);
         }
     }
 }
