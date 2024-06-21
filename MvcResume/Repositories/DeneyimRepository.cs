8b3eeb679971c5d6e851a8889d91fab2319058af
aliwert <154356044+aliwert@users.noreply.github.com>
2024-06-21 22:58:18 +0300

    generic structure

---
+++ MvcResume/Repositories/DeneyimRepository.cs
@@ -0,0 +1,13 @@
+﻿using MvcResume.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcResume.Repositories
+{
+    public class DeneyimRepository
+    {
+       
+    }
+}
---
+++ MvcResume/Repositories/GenericRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MvcResume.Models.Entity;
+
+namespace MvcResume.Repositories
+{
+    public class GenericRepository<T> where T : class, new()
+    {
+        DbCvEntities db = new DbCvEntities();
+
+        public List<T> List()
+        {
+           return db.Set<T>().ToList();
+        }
+        public void TAdd(T p)
+        {
+            db.Set<T>().Add(p);
+            db.SaveChanges();
+        }
+        public void Tdelete(T p)
+        {
+            db.Set<T>().Remove(p);
+            db.SaveChanges();
+        }
+        public T TGet(int id)
+        {
+            return db.Set<T>().Find(id);
+        }
+        public void TUpdate(T p)
+        {
+            db.SaveChanges();
+        }
+
+    }
+}
