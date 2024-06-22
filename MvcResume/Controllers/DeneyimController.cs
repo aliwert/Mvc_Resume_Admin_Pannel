ed47c8c8e69b942bdb1d350afc055b7557d2a927
aliwert <154356044+aliwert@users.noreply.github.com>
2024-06-22 17:23:45 +0300

    experience

---
+++ MvcResume/Controllers/DeneyimController.cs
@@ -0,0 +1,20 @@
+﻿using MvcResume.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcResume.Controllers
+{
+    public class DeneyimController : Controller
+    {
+        // GET: Deneyim
+        DeneyimRepository dp = new DeneyimRepository();
+        public ActionResult Index()
+        {
+            var deneyimler = dp.List();
+            return View(deneyimler);
+        }
+    }
+}
