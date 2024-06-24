7574e9f8d76733c99e78c567327ea064e91db57f
aliwert <154356044+aliwert@users.noreply.github.com>
2024-06-24 13:33:07 +0300

    education section

---
+++ MvcResume/Controllers/EgitimController.cs
@@ -0,0 +1,22 @@
+﻿using MvcResume.Models.Entity;
+using MvcResume.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcResume.Controllers
+{
+    public class EgitimController : Controller
+    {
+        // GET: Egitim
+        GenericRepository<TblEgitim> repo = new GenericRepository<TblEgitim>();
+        public ActionResult Index()
+        {
+            var egitim = repo.List();
+
+            return View(egitim);
+        }
+    }
+}
