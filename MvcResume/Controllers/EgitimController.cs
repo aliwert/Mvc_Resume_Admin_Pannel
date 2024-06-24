e8d1135fb8cf896d61ab3e6a6ba876ffab4709a2
aliwert <154356044+aliwert@users.noreply.github.com>
2024-06-24 19:16:14 +0300

    edit educations

--- MvcResume/Controllers/EgitimController.cs
+++ MvcResume/Controllers/EgitimController.cs
@@ -40,6 +40,28 @@ namespace MvcResume.Controllers
             repo.Tdelete(egitim);
             return RedirectToAction("Index");
         }
+        [HttpGet]
+        public ActionResult EditEducation(int id)
+        {
+            var egitim = repo.Find(x => x.ID == id);
+            return View(egitim);
+        }
+        [HttpPost]
+        public ActionResult EditEducation(TblEgitim t)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("EditEducation");
+            }
+            var egitim = repo.Find(x => x.ID == t.ID);
+            egitim.Baslik = t.Baslik;
+            egitim.AltBaslik1 = t.AltBaslik1;
+            egitim.AltBaslik2 = t.AltBaslik2;
+            egitim.Tarih = t.Tarih;
+            egitim.GNO = t.GNO;
+            repo.TUpdate(egitim);
+            return RedirectToAction("Index");
+        }
 
     }
 }
