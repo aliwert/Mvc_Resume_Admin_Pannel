22d051a8034d66dafa6288bfccc85a756efd3ab2
aliwert <154356044+aliwert@users.noreply.github.com>
2024-06-25 23:50:37 +0300

    created certificate controller

--- MvcResume/Controllers/SertifikaController.cs
+++ MvcResume/Controllers/SertifikaController.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcResume.Models.Entity;
+using MvcResume.Repositories;
 
 namespace MvcResume.Controllers
 {
     public class SertifikaController : Controller
     {
         // GET: Sertifika
+
+        GenericRepository<TblSertifikalarim> repo = new GenericRepository<TblSertifikalarim> ();
         public ActionResult Index()
         {
-            return View();
+            var sertifika = repo.List();
+            return View(sertifika);
         }
     }
 }
