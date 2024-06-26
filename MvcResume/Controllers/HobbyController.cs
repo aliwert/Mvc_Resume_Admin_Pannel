d866b2bc9348751b49b3013caab2372c83f5c7b0
aliwert <154356044+aliwert@users.noreply.github.com>
2024-06-26 22:44:40 +0300

    hobbies added

---
+++ MvcResume/Controllers/HobbyController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcResume.Models.Entity;
+using MvcResume.Repositories;
+namespace MvcResume.Controllers
+{
+    public class HobbyController : Controller
+    {
+        // GET: Hobby
+        GenericRepository<TblHobilerim> repo = new GenericRepository<TblHobilerim>();
+        public ActionResult Index()
+        {
+            var hobbies = repo.List();
+            return View(hobbies);
+        }
+    }
+}
