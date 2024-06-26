43380b806a3e4037c4f3000d3a076ac208aac9a6
aliwert <154356044+aliwert@users.noreply.github.com>
2024-06-26 23:07:52 +0300

    aboutcontroller added

--- MvcResume/Controllers/HakkimdaController.cs
+++ MvcResume/Controllers/HakkimdaController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MvcResume.Models.Entity;
+using MvcResume.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,28 @@ namespace MvcResume.Controllers
     public class HakkimdaController : Controller
     {
         // GET: Hakkimda
+        DbCvEntities db = new DbCvEntities();
+
+        GenericRepository<TblAbout> repo = new GenericRepository<TblAbout>();
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            var about = repo.List();
+            return View(about);
+        }
+        [HttpPost]
+        public ActionResult Index(TblAbout p)
+        {
+            var h = repo.Find(x => x.ID == 1);
+            h.Name = p.Name;
+            h.Surname = p.Surname;
+            h.Adress = p.Adress;
+            h.Mail = p.Mail;
+            h.Phone = p.Phone;
+            h.Description = p.Description;
+            h.Fotograf = p.Fotograf;
+            repo.TUpdate(h);
+            return RedirectToAction("Index");
         }
     }
 }
