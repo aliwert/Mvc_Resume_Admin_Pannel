1884e21d73b5c0f47094dd01d323b6dccc1e90e6
aliwert <154356044+aliwert@users.noreply.github.com>
2024-06-26 19:45:23 +0300

    contact controller message list

--- MvcResume/Controllers/ContactController.cs
+++ MvcResume/Controllers/ContactController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcResume.Models.Entity;
+using MvcResume.Repositories;
 
 namespace MvcResume.Controllers
 {
     public class ContactController : Controller
     {
-        // GET: Contact
+        GenericRepository<TblIletisim> repo = new GenericRepository<TblIletisim>();
         public ActionResult Index()
         {
-            return View();
+            var message = repo.List();
+            return View(message);
         }
     }
 }
