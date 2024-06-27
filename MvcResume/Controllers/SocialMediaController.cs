3af3b6a22b2525560ff4a00f25ca994664a56b18
aliwert <154356044+aliwert@users.noreply.github.com>
2024-06-27 23:51:09 +0300

    bug fixed

---
+++ MvcResume/Controllers/SocialMediaController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcResume.Models.Entity;
+using MvcResume.Repositories;
+
+namespace MvcResume.Controllers
+{
+    public class SocialMediaController : Controller
+    {
+        // GET: SocialMedia
+        GenericRepository<TblSosyalMedya> repo = new GenericRepository<TblSosyalMedya>();
+        public ActionResult Index()
+        {
+            var veriler = repo.List();
+            return View(veriler);
+        }
+    }
+}
