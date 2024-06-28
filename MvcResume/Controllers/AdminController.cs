7051a52c306c867412826a5a05df7c49c21c4f01
aliwert <154356044+aliwert@users.noreply.github.com>
2024-06-28 14:39:45 +0300

    admin controller

---
+++ MvcResume/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcResume.Models.Entity;
+using MvcResume.Repositories;
+
+namespace MvcResume.Controllers
+{
+    [AllowAnonymous]
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        GenericRepository<TblAdmin> repo = new GenericRepository<TblAdmin>();
+        public ActionResult Index()
+        {
+            var liste = repo.List();
+            return View(liste);
+        }
+    }
+}
