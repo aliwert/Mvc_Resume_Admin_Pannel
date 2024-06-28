d3805165a0a03d474ff82e5192eeec7861e9f170
aliwert <154356044+aliwert@users.noreply.github.com>
2024-06-28 13:42:18 +0300

    login controller & login page

--- MvcResume/Controllers/LoginController.cs
+++ MvcResume/Controllers/LoginController.cs
@@ -1,17 +1,36 @@
-﻿using System;
+﻿using MvcResume.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace MvcResume.Controllers
 {
     public class LoginController : Controller
     {
         // GET: Login
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Index(TblAdmin p)
+        {
+            DbCvEntities db = new DbCvEntities();
+            var bilgi = db.TblAdmins.FirstOrDefault(x=> x.KullaniciAdi== p.KullaniciAdi && x.Sifre == p.Sifre);
+            if (bilgi != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgi.KullaniciAdi, false);
+                Session["KullaniciAdi"] = bilgi.KullaniciAdi.ToString();
+                return RedirectToAction("Index", "Education");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }
         }
     }
 }
